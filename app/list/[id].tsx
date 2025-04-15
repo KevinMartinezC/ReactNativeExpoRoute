2be3241bbc17d3b3da2ecebf169e0037e3df0e66
kevinmartinezc <ceronk003@gmail.com>
2025-04-15 14:38:25 -0600

    fix: id getting undefined

--- app/list/[id].tsx
+++ app/list/[id].tsx
@@ -1,11 +1,10 @@
-
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useSearchParams } from 'expo-router/build/hooks'
+import { useLocalSearchParams } from 'expo-router'
 import { Stack } from 'expo-router'
 
 const DetailsPage = () => {
-    const { id } = useSearchParams()
+    const { id } = useLocalSearchParams<{ id: string }>()
   return (
     <View>
       <Stack.Screen options={{ title: `Details ${id}` }} />
